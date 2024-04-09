c1b199810ce5828e9891f45f7fc4e781a1c0f30d
Jeajoe11 <148391783+Jeajoe11@users.noreply.github.com>
2024-04-08 20:32:15 -0400

    dsfa

--- Home Page/script.js
+++ Home Page/script.js
@@ -1,10 +1,10 @@
 let myUser = window.prompt("Entrez votre nom");
 const myName = document.getElementById("myName");
-while(myUser === ""){
+do{
   myUser = window.prompt("Entrez votre nom");
   myName.textContent = "[ANONYME]";
   myName.setAttribute("title", "Veuillez actualiser la page pour changer de nom")
-}
+}while(myUser === "")
 if(myUser === "Créateur"){
   myName.textContent = "Joël";
   myName.setAttribute("title", myUser)
