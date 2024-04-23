30bce4860c2614109264b448aa243e2f11d84356
Jeajoe11 <148391783+Jeajoe11@users.noreply.github.com>
2024-04-23 13:13:19 -0400

    dd

--- Home Page/script.js
+++ Home Page/script.js
@@ -5,30 +5,25 @@ const Container2 = document.getElementById("Container2");
 
 while(myUser === "" || myUser === null){
   myUser = window.prompt("Entrez votre nom");
-  myName.textContent = "[ANONYME]";
-  myName.setAttribute("title", "Veuillez actualiser la page pour changer de nom");
+  myName.textContent = "[ANONYME]".setAttribute("title", "Veuillez actualiser la page pour changer de nom");
 
-  myName2.textContent = "[ANONYME]";
-  myName2.setAttribute("title", "Veuillez actualiser la page pour changer de nom");
+  myName2.textContent = "[ANONYME]".setAttribute("title", "Veuillez actualiser la page pour changer de nom");
 };
 if(!myUser === "" || myUser === null){
   Container2.setAttribute("data-inserted", "");
+
 }
 else if(myUser === "Créateur"){
-  myName.textContent = "Joël";
-  myName.setAttribute("title", myUser);
+  myName.textContent = "Joël".setAttribute("title", myUser).setAttribute("capital", "");
 
-  myName2.textContent = "Joël";
-  myName2.setAttribute("title", myUser);
+  myName2.textContent = "Joël".setAttribute("title", myUser).setAttribute("capital", "");
 
   Container2.setAttribute("data-inserted", "");
 }
 else{
-  myName.textContent = myUser;
-  myName.setAttribute("title", myUser);
+  myName.textContent = myUser.setAttribute("title", myUser).setAttribute("capital", "");
 
-  myName2.textContent = myUser;
-  myName2.setAttribute("title", myUser);
+  myName2.textContent = myUser.setAttribute("title", myUser).setAttribute("capital", "");
 
   Container2.setAttribute("data-inserted", "");
 };
